601336014b788b14be17cef3f4cbad22dac62c9b
MATHEUS RODRIGUES MENDONÇA <82005382+matheusrodrinks@users.noreply.github.com>
2023-02-09 13:08:46 -0400

    lidando com erros com throw

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -19,13 +19,25 @@ async function criaVideo(titulo, descricao, url, imagem) {
         })
     })
     
-    const conexaoConvertida = await conexao.json();
+    if(!conexao.ok){
+        throw new Error("Não foi possível enviar o vídeo");
+    }
+    const conexaoConvertida = conexao.json();
+    return conexaoConvertida;
+}
+
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = conexao.json();
+
     return conexaoConvertida;
 }
 
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
 }
 
 
